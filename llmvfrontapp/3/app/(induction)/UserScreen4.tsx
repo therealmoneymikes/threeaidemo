db979717e346b06ba1024b316c310e9c14b50683
Michael Roberts - CSAcademyIO <m.roberts94@hotmail.co.uk>
2025-01-12 20:56:24 +0000

    Add User Screen 4 - Age Checker

---
+++ llmvfrontapp/3/app/(induction)/UserScreen4.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import BaseScreen from "@/components/basecomponents/BaseScreen";
+import CustomPressable from "@/components/basecomponents/CustomPressable";
+import colours from "@/config/colours";
+import { spacingY } from "@/config/spacings";
+
+
+
+const ageRanges = ["18 or under", "19-24", "25-34", "35-44", "45-over"];
+
+const UsersAge = () => {
+     const handleNextScreen = () => {
+       console.log("move to screen 5");
+     };
+
+  return (
+    <BaseScreen animateBackground gradient>
+  
+      <View
+        style={{
+          alignSelf: "center",
+          justifyContent: "center",
+          width: "90%",
+          flex: 1,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 32,
+            color: "white",
+            alignSelf: "flex-start",
+            fontWeight: "600",
+            paddingBottom: spacingY._10,
+          }}
+        >
+          What best describes you at the moment?
+        </Text>
+        {ageRanges.map((item, index) => (
+          <CustomPressable
+            onPress={() => handleNextScreen()}
+            key={index}
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              borderWidth: 1,
+              padding: 10,
+              height: "10%",
+              maxHeight: 200,
+              borderRadius: 10,
+              borderColor: colours.csaGray,
+              width: "100%",
+              marginVertical: spacingY._10,
+              gap: 10,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 28,
+                fontFamily: "Nunito",
+                color: "white",
+              }}
+            >
+              {item}
+            </Text>
+          </CustomPressable>
+        ))}
+      </View>
+    </BaseScreen>
+  );
+};
+
+export default UsersAge;
+
+const styles = StyleSheet.create({});
