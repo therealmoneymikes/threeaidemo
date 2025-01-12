17141befa2aa89fc18592e05eee0557097f2bcd0
Michael Roberts - CSAcademyIO <m.roberts94@hotmail.co.uk>
2025-01-12 18:59:54 +0000

    Add __init__.py initialiser with  Chatbot Agents initialisation

---
+++ llmvbackend/api/agents/__init__.py
@@ -0,0 +1,12 @@
+"""
+        Activate Chatbot Agents 
+        on Module initialsation
+        
+"""
+
+from .main_protocol import MainAgentProtocol
+from .input_guard_agent import GuardAgent
+from .recommendation_agent import RecommendationAgent
+from .classification_agent import ClassificationAgent
+from .orders_agent import OrderTakingAgent
+from .details_agent import DetailsAgent
